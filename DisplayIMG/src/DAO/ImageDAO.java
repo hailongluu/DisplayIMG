676a24918e241e148f696a6889fb1931a152b92b
Long Luu <hailongluu@gmail.com>
2018-06-29 16:13:58 +0700

    s

--- DisplayIMG/src/DAO/ImageDAO.java
+++ DisplayIMG/src/DAO/ImageDAO.java
@@ -14,14 +14,15 @@ import java.sql.ResultSet;
  * @author hailo
  */
 public class ImageDAO {
-    public boolean addIMG(String link){
+
+    public boolean addIMG(String link) {
         try {
             Connection con = new DBConnection().getConnection();
             con.setAutoCommit(false);
             String sql = "INSERT INTO `image` (`link`) VALUES (?);";
             PreparedStatement pre = (PreparedStatement) con.prepareStatement(sql);
             pre.setString(1, link);
-            if(pre.executeUpdate()==1){
+            if (pre.executeUpdate() == 1) {
                 con.commit();
                 return true;
             }
@@ -29,21 +30,39 @@ public class ImageDAO {
         }
         return false;
     }
-    public String getIMG(int id){
-        String link="";
-         try {
+
+    public String getIMG(int id) {
+        String link = "";
+        try {
             Connection con = new DBConnection().getConnection();
             con.setAutoCommit(false);
             String sql = "SELECT * FROM image WHERE id=?;";
             PreparedStatement pre = (PreparedStatement) con.prepareStatement(sql);
             pre.setInt(1, id);
-             ResultSet rs = pre.executeQuery();
-             while(rs.next()){
-                 link = rs.getString("link");
-             }
-             return link;
+            ResultSet rs = pre.executeQuery();
+            while (rs.next()) {
+                link = rs.getString("link");
+            }
+            return link;
         } catch (Exception e) {
         }
         return link;
+    }
+
+    public int getNumber() {
+        int number = 0;
+        try {
+            Connection con = new DBConnection().getConnection();
+            con.setAutoCommit(false);
+            String sql = "SELECT count(*) FROM image ;";
+            PreparedStatement pre = (PreparedStatement) con.prepareStatement(sql);
+            ResultSet rs = pre.executeQuery();
+            while (rs.next()) {
+                 number = rs.getInt(1);
+            }
+            return number;
+        } catch (Exception e) {
+        }
+        return number;
     }
 }
